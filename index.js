33f37754c2ef93aeb28c0ca5b818fd5278f6cfdf
camper2018 <taniafali@gmail.com>
2021-01-06 01:05:04 -0800

    Node Callbacks and Error Handling

--- index.js
+++ index.js
@@ -4,20 +4,30 @@ function solveRect(length, breadth) {
   console.log(
     "Solving for rectangle with length =  " + length + " breadth =  " + breadth
   );
-  if (length <= 0 || breadth <= 0) {
-    console.log(
-      "Rectangle dimensions should be greater than zero:length =  " +
-        length +
-        " breadth = " +
-        breadth
-    );
-  } else {
-    console.log("The area of the rectangle is " + rect.area(length, breadth));
-    console.log(
-      "The perimeter of the rectangle is " + rect.perimeter(length, breadth)
-    );
-  }
+  rect(length, breadth, (err, rectangle) => {
+    if (err) {
+      console.log("ERROR: ", err.message);
+    } else {
+      console.log(
+        "The area of the rectangle of dimensions length =  " +
+          length +
+          " and breadth = " +
+          breadth +
+          " is " +
+          rectangle.area()
+      );
+      console.log(
+        "The perimeter of the rectangle of dimensions length =  " +
+          length +
+          " and breadth = " +
+          breadth +
+          " is " +
+          rectangle.perimeter()
+      );
+    }
+  });
 }
+console.log("This statement is after the call to rect()");
 solveRect(2, 4);
 solveRect(3, 5);
 solveRect(0, 5);
