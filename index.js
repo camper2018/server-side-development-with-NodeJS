d97334b44be670f4a5cd31dd5deb8cce2dd2eced
camper2018 <taniafali@gmail.com>
2021-01-08 20:18:21 -0800

    Express Serve Static Files

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const http = require("http");
+const morgan = require("morgan");
 
 const hostname = "localhost";
 const port = 3000;
 
 const app = express();
+app.use(morgan("dev"));
+
+app.use(express.static(__dirname + "/public"));
+// This is informing Express that you will look at this particular folder in the root folder of this project and inside the public folder.
 
 app.use((req, res, next) => {
   // Express uses additional middleware so, the next is used when you need to invoke additional middleware to take care of work on your behalf.
   // Next is an optional parameter that can be not included if you are not going to use it within your code.
-  console.log(req.headers);
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
   res.end("<html><body><h1>This is an Express Server</h1></body></html>");
