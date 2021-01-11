e783464daaf19cb7815b8a92a93520c9951bfe1a
camper2018 <taniafali@gmail.com>
2021-01-10 19:09:06 -0800

    Express Router

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const dishRouter = require("./routes/dishRouter");
 const hostname = "localhost";
 const port = 3000;
 
@@ -10,41 +11,8 @@ const app = express();
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 // when we use the body parser middleware, it parses the body of the incoming request and then adds it on to the request in json format as req.body object.
-app.all("/dishes", (req, res, next) => {
-  // When we say app.all, no matter which method is invoked, get, put, post, or delete, for the /dishes REST API endpoint,
-  // this code will be executed first by default here.
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  next();
-  // So when you call next, what it means is that it'll continue on to look for additional specifications that match the dishes endpoint.
-  // So this would be done for all the requests, get, put, post, and delete, on the dishes,
-  // and it'll continue on to the next one with /dishes endpoint as below.
-});
-
-app.get("/dishes", (req, res, next) => {
-  // will receive req processed by next() method in app.all().
-  res.end("Will send all the dishes to you!");
-  // This will send all dishes from mongodb database.
-});
-app.post("/dishes", (req, res, next) => {
-  // will receive req processed by next() method in app.all().
-  res.end(
-    "Will add the dish: " +
-      req.body.name +
-      " with details: " +
-      req.body.description
-  );
-});
-app.put("/dishes", (req, res, next) => {
-  // will receive req processed by next() method in app.all().
-  res.statusCode = 403;
-  res.end("PUT operation not supported on /dishes");
-});
-app.delete("/dishes", (req, res, next) => {
-  // will receive req processed by next() method in app.all().
-  res.end("Deleting all the dishes!");
-  // This will send all dishes from mongodb database.
-});
+app.use("/dishes", dishRouter);
+// any request coming to /dishes endpoint will be handled by dishRouter module. As dishRouter.route has been defined as "/" which means it is mounted at "/dishes" endpoint.
 app.get("/dishes/:dishId", (req, res, next) => {
   res.end("Will send details of the dish: " + req.params.dishId + " to you!");
 });
@@ -64,9 +32,13 @@ app.put("/dishes/:dishId", (req, res, next) => {
 app.delete("/dishes/:dishId", (req, res, next) => {
   res.end("Deleting dish! " + req.params.dishId);
 });
-
 app.use(express.static(__dirname + "/public"));
 // This is informing Express that you will look at this particular folder in the root folder of this project and inside the public folder.
+// Will server static html file in public folder,
+// and by default it will serve index.html,
+// otherwise will serve the specidfied file in the url.
+// For example: localhost:3000/aboutus.html will serve aboutus.html and,
+// localhost:3000/index.html will serve index.html.
 
 app.use((req, res, next) => {
   // Express uses additional middleware so, the next is used when you need to invoke additional middleware to take care of work on your behalf.
