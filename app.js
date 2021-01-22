f7c1f912a7bf2e94852e30753551997c740245e9
camper2018 <taniafali@gmail.com>
2021-01-22 12:17:21 -0800

    Basic Authentication

--- app.js
+++ app.js
@@ -27,11 +27,43 @@ var app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+// auth middleware
+const auth = (req, res, next) => {
+  console.log(req.headers);
+  // The header will have no authorization field when first request is received.
+  // We will set authorization header if it is not found so that subsequent requests contain this header.
+  // Also will send an error message to the client with status of Unauthorized.
+  let authHeader = req.headers.authorization;
+  if (!authHeader) {
+    let err = new Error("You are not authenticated!");
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    // passing error to next() will send this error msg to the error handler below.
+    next(err);
+    return;
+  } else {
+    let [user, password] = new Buffer.from(authHeader.split(" ")[1], "base64")
+      .toString()
+      .split(":");
+    // here we split the authorization header at space that gives us ['Basic', 'base64 encoded string' ]
+    // We futher split base64 string at ':' to get [username, password]
+    if (user === "admin" && password === "password") {
+      // if authentication is successful, we will send the request object to be handled by subsequent middlewares.
+      next();
+    } else {
+      let err = new Error("You are not authenticated!");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      next(err);
+    }
+  }
+};
+// We are using our auth middleware here right after other middlewares and before express serving static files as we want to serve only to the authenticated users.
+app.use(auth);
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
